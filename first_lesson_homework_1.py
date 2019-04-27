406e2a3fdbff0f454a825ee51e33d3a52837cdd0
Taras Ziniak <taras193@gmail.com>
2019-04-28 00:23:03 +0300

    Upload homework files

---
+++ first_lesson_homework_1.py
@@ -0,0 +1,10 @@
+print ("Hello,")
+name=input("What is your name? \n")
+age=input("How old are you? ")
+adress=input("Where do you live? ")
+
+print ("Hello,", name)
+print ("Your age is ",age)
+print ("You live in ",adress)
+
+import this
