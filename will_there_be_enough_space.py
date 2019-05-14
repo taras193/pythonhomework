b592ddc8d2a11df2065be02fa750c41d00230102
Taras Ziniak <taras193@gmail.com>
2019-05-14 13:47:37 +0300

    Updated Will be there enough space?

--- will_there_be_enough_space.py
+++ will_there_be_enough_space.py
@@ -1,3 +1,8 @@
+#Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents.
+#With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! 
+# He wants you to write a simple program telling him if he will be able to fit all the passengers.
+#If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
 def enough(cap, on, wait):
     if (cap-on)>wait:
         return 0
