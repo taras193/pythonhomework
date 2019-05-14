f00dfb2643968ac15d43f43e97e797ab20544535
Taras Ziniak <taras193@gmail.com>
2019-05-14 13:51:48 +0300

    Updated COnvert boolean

--- convert_boolean_values_to_strings.py
+++ convert_boolean_values_to_strings.py
@@ -1,3 +1,4 @@
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 def bool_to_word(boolean):
     if boolean == 1 :
         return 'Yes'
