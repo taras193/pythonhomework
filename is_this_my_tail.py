eabe9d174d9a3ee67d6e8ef057f21fe3b094182a
Taras Ziniak <taras193@gmail.com>
2019-05-14 14:22:04 +0300

    Updated Loop fix

--- is_this_my_tail.py
+++ is_this_my_tail.py
@@ -2,10 +2,8 @@
 # The zoo keeper is concerned that perhaps the animals do not have the right tails. 
 # To help her, you must correct the broken function to make sure that the second argument (tail), 
 # is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
-
-If the tail is right return true, else return false.
-
-The arguments will always be strings, and normal letters.
+#If the tail is right return true, else return false.
+#The arguments will always be strings, and normal letters.
 def correct_tail(body, tail):
     sub = body[len(body)-len(tail):]
     if tail == sub:
