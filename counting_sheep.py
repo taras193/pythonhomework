ec6464f528c5b4340cb52682312c9517fc1aef9e
Taras Ziniak <taras193@gmail.com>
2019-05-14 13:53:30 +0300

    Updated Count sheep

--- counting_sheep.py
+++ counting_sheep.py
@@ -1,3 +1,5 @@
+#Consider an array of sheep where some sheep may be missing from their place. 
+#We need a function that counts the number of sheep present in the array (true means present).
 def count_sheeps(arrayOfSheeps):
     a=int(0)
     for x in (arrayOfSheeps):
