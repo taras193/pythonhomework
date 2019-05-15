1c0edeee6d2bb44951c8bb04656fad07190e07d6
Taras Ziniak <taras193@gmail.com>
2019-05-15 18:52:32 +0300

    Updatrd Classwork 02.05

--- Classwork 02.05.2019.py
+++ Classwork 02.05.2019.py
@@ -31,9 +31,75 @@
 #     print(i, end=' ')
 
 #brake#
-spisok = (2,4,5,6,7,8,9,10)
-for item in spisok:
-    if i % 2 ==1:
-    break
-print ("There is odd number in the list") if i % 2 ==1
+# spisok = (2,4,5,6,7,8,9,10)
+# for item in spisok:
+#     if i % 2 ==1:
+#     break
+# print ("There is odd number in the list") if i % 2 ==1
 
+# 1.  Роздрукувати всі парні числа менші 100 (написати два варіанти коду: один використовуючи цикл while, а інший з використанням циклу for).
+# 1.a
+# nums = []
+# for x in range (1,101):
+#     if x%2 == 0:
+#         nums.append(x)
+# print(nums)
+
+# 1.b
+# nums = []
+# i = 0
+# while i <= 100:
+#     i += 1
+#     if i%2 == 0:
+#         nums.append(i)
+# print(nums)
+
+# 2. Роздрукувати всі непарні числа менші 100. (написати два варіанти коду: один використовуючи оператор continue, а інший без цього оператора).
+# # 2.a
+# odd = []
+# for x in range (1,101):
+#     if x%2 != 0:
+#         odd.append(x)
+#         continue
+# print(odd)
+# # 2.b
+# odd2 = []
+# i = 1
+# while i > 0:
+#     odd2.append(i)
+#     i += 2
+#     if i >= 100:
+#         break
+# print(odd2)
+
+# # 3.Перевірити чи список містить непарні числа.
+# # використати оператор break
+# listnumm = (6, 4, 8, 6, 5, 4)
+# for y in listnumm:
+#     if y%2 != 0:
+#         print("Has odd numbers")
+#         break
+# else: 
+#     print("Has only even numbers")
+
+# # 4. Створити список, який містить елементи цілочисельного типу, потім за допомогою циклу перебору змінити тип даних елементів на числа з плаваючою крапкою. 
+# # (Підказка: використати вбудовану функцію float ()).
+# listfloat = []
+# for integ in listnumm:
+#     listfloat.append(float(integ))
+# print(listfloat)
+
+# 5. Вивести числа Фібоначі включно до введеного числа n, використовуючи цикли. (Послідовність чисел Фібоначі 0, 1, 1, 2, 3, 5, 8, 13 і т.д.)
+#TO DO
+
+# 6. Створити список, що складається з чотирьох елементів типу string. Потім, за допомогою циклу for, вивести елементи по черзі на екран.
+# listforprint = ["element1","element2","element3","element4"]
+# for x in listforprint:
+#     print(x)
+
+# 7. Змінити попередню програму так, щоб в кінці кожної букви елементів при виводі додавався певний символ, наприклад “#”.
+# symb = "'"
+# for x in listforprint:
+#     for y in x:
+#         print(y, end=symb)
+#     print()
