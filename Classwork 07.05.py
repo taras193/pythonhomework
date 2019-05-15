fce2ecf584ffc0961eed332db8c813fd834b345e
Taras Ziniak <taras193@gmail.com>
2019-05-15 19:20:07 +0300

    Updates classwork 07.05

--- Classwork 07.05.py
+++ Classwork 07.05.py
@@ -25,29 +25,45 @@
 # print (maximum_number.__doc__)
 
 #4 
-PI=3.14
-t=input(int("""Enter please a number of figure you want to calcute:
-1:triangle
-2:square
-3:circle"""))
-def triangle_area(a, h):
-    a=float(input('input base'))
-    h=float(input('input high'))
-    a=0.5*a*h
-    print ("Triangle are is ={}" .format (0.5*a*h))
+# PI=3.14
+# t=input(int("""Enter please a number of figure you want to calcute:
+# 1:triangle
+# 2:square
+# 3:circle"""))
+# def triangle_area(a, h):
+#     a=float(input('input base'))
+#     h=float(input('input high'))
+#     a=0.5*a*h
+#     print ("Triangle are is ={}" .format (0.5*a*h))
 
-def square_area(x,y):
-    a=a*b
-    return a
+# def square_area(x,y):
+#     a=a*b
+#     return a
 
-def circle_area(r)
-    a=PI * r**2
-    return a
+# def circle_area(r)
+#     a=PI * r**2
+#     return a
 
-def area(t)
-    if t=1:
-        triangle_area()
-    elif: t=2 
-        square_area()
-    elif: t=3 
-        circle_area()
+# def area(t)
+#     if t=1:
+#         triangle_area()
+#     elif: t=2 
+#         square_area()
+#     elif: t=3 
+#         circle_area()
+
+# 5.  Написати функцію, яка обчислює суму цифр введеного числа.
+# numbers = input("Please enter your number:\n")
+# def sumval (a):
+#     x = 0
+#     for num in a:
+#         x += int(num)
+#     return x
+
+# print("Sum of numbers: "+str(sumval(numbers)))
+
+# 6.  Написати програму калькулятор, яка складається з наступних функцій: 
+
+# головної, яка пропонує вибрати дію та додаткових, які реалізовують вибрані дії, калькулятор працює доти, поки ми не виберемо дію вийти з калькулятора, після виходу, користувач отримує повідомлення з подякою за вибір нашого програмного продукту!!!
+
+TO DO
